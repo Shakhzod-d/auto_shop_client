46a231e62b243d0b0b4379e0459ebafd6c09a7af
bekruz-pardayev <behruzp330@gmail.com>
2025-03-12 05:54:02 +0500

    langue pathname error

--- components/ui/langueSelect.tsx
+++ components/ui/langueSelect.tsx
@@ -4,8 +4,6 @@ import { useState, useRef, useEffect } from "react";
 import { ChevronDown } from "lucide-react";
 import { cn } from "../../lib/utils";
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 type Language = {
   code: string;
@@ -37,7 +35,6 @@ interface LanguageSelectorProps {
 
 export default function LanguageSelector({ onChange }: LanguageSelectorProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
   const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
@@ -96,20 +93,8 @@ export default function LanguageSelector({ onChange }: LanguageSelectorProps) {
                 role="menuitem"
                 onClick={() => handleLanguageChange(language)}
               >
-                <Link
-                  key={language.code}
-                  href={`/${language.code}${pathname.replace(
-                    /^\/(en|uz|ru)/,
-                    ""
-                  )}`}
-                >
-                  <Image
-                    src={language.flag}
-                    width={20}
-                    height={20}
-                    alt="flag"
-                  />
-                </Link>
+                <Image src={language.flag} width={20} height={20} alt="flag" />
+
                 <span className="font-medium">{language.name}</span>
               </button>
             ))}
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["en", "uz", "ru"],
-    defaultLocale: "uz",
-  },
   images: {
     domains: ["autoshop.fra1.cdn.digitaloceanspaces.com"],
   },
