d0901d7a7fa343219b28f73b8d99c3d30e30253e
bekruz-pardayev <behruzp330@gmail.com>
2025-03-19 05:27:42 +0500

    auth code apitmizatsiyon

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -7,27 +7,30 @@ import { Register } from "./components/register";
 import { Success } from "./components/success";
 import { useAuth } from "../../store/auth-store";
 import { Login } from "./components/login";
+import { JSX } from "react";
 
 export default function Auth() {
   const { success, successType, authType } = useAuth();
+  const obj: Record<string, JSX.Element> = {
+    true: <Success variant={successType} />,
+    false: (
+      <div className="container flex justify-between  flex-col  xl:flex-row ">
+        {authType == "register" ? <Register /> : <Login />}
+        <div className="w-full flex justify-center mb-[50px] lg:m-0">
+          <Image
+            src={"/imgs/auth-bg.png"}
+            alt="auth picture"
+            width={570}
+            height={450}
+          />
+        </div>
+      </div>
+    ),
+  };
   return (
     <>
       <Header />
-      {success ? (
-        <Success variant={successType} />
-      ) : (
-        <div className="container flex justify-between  flex-col  xl:flex-row ">
-          {authType == "register" ? <Register /> : <Login />}
-          <div className="w-full flex justify-center mb-[50px] lg:m-0">
-            <Image
-              src={"/imgs/auth-bg.png"}
-              alt="auth picture"
-              width={570}
-              height={450}
-            />
-          </div>
-        </div>
-      )}
+      {obj[String(success)]}
     </>
   );
 }
