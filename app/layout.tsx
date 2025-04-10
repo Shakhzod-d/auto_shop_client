996e599c6fde7204b38d44b68bc7f74e32e8719a
bekruz-pardayev <behruzp330@gmail.com>
2025-04-10 15:15:31 +0500

    font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Montserrat, Lora, Merriweather } from "next/font/google";
+import { Montserrat, Lora,  Poppins } from "next/font/google";
 import "./index.css";
 import AppLayout from "./app-layout";
 import QueryProvider from "../providers/query-provider";
 
-
 export const metadata: Metadata = {
   title: "Avtomobil texnologiyalari, elektifikatsiya va innovatsiyalar",
-  description: "Autoshop.uz - Avtomobil texnologiyalari, EV, elektifikatsiya va innovatsiyalar haqida eng so'nggi yangiliklar va trendlar. Elektr avtomobillari, sun'iy intellekt, va ilg'or texnologiyalar bizning ",
+  description:
+    "Autoshop.uz - Avtomobil texnologiyalari, EV, elektifikatsiya va innovatsiyalar haqida eng so'nggi yangiliklar va trendlar. Elektr avtomobillari, sun'iy intellekt, va ilg'or texnologiyalar bizning ",
 };
 
 const montserrat = Montserrat({
@@ -22,7 +22,7 @@ const lora = Lora({
   variable: "--font-lora",
 });
 
-const merriweather = Merriweather({
+const merriweather = Poppins({
   weight: ["300", "400", "400", "700", "900"],
   subsets: ["latin"],
   variable: "--font-merriweather",
